ec1684857cad3c659f7c27f2822aa5f87ffa9d2c
rajnish7250 <106313846+rajnish7250@users.noreply.github.com>
2026-08-03 14:20:38 +0530

    chore: configuring logging to use local timezone

--- utils/logging_config.py
+++ utils/logging_config.py
@@ -1,8 +1,11 @@
 import logging
+import time
+logging.Formatter.converter = time.localtime
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
 )
 
 # Reduce noise from third-party libraries
